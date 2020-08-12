8cbdd6bb204da654a2f58e1c7fb100aec36f4650
Asher <asher_3@hotmail.co.uk>
2020-08-12 07:10:51 +0100

    duplicate folders editing

--- src/components/ui/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import spinner from "../../images/spinner.svg";
-
-const Spinner = () => {
-  return <img src={spinner} style={{ width: "200px", margin: "auto", display: "block" }} alt="Loading" />;
-};
-
-export default Spinner;
